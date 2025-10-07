3f8900e8f882e8322ba67a75060252456588101b
juPi21 <denzybit@gmail.com>
2025-10-07 14:58:21 +0300

    init commit mtf dih

---
+++ src/test1/class1.java
@@ -0,0 +1,7 @@
+package test1;
+
+public class class1 {
+    public static void main(String[] arg) {
+    	System.out.println("Hello World!!!!");
+    }
+}
---
+++ src/test1/class2.java
@@ -0,0 +1,7 @@
+package test1;
+
+public class class2 {
+ public static void main(String[] arg) {
+	 System.out.println("Hello World");
+ }
+}
